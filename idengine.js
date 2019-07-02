6e332381a8f977c171ccc23488e3ba0b62c8ac0e
turinglabsorg <turinglabs@tuta.io>
2019-07-02 15:58:10 +0200

    sign module

--- idengine.js
+++ idengine.js
@@ -7,16 +7,8 @@ app.use(require('serve-static')(__dirname + '/public'))
 app.use(require('cookie-parser')())
 app.use(require('body-parser').urlencoded({ extended: true }))
 app.use(passport.initialize())
-const port = 3000
-var CoinKey = require('coinkey')
-const CryptoJS = require('crypto-js')
-const secp256k1 = require('secp256k1')
-
-const lyraInfo = {
-    private: 0xae,
-    public: 0x30,
-    scripthash: 0x0d
-};
+const port = process.env.ENGINE_PORT
+const sign = require('./sign.js')
 
 var GitHubStrategy = require('passport-github').Strategy;
 var privKeyTest = 'Spv2RHfg2sheqCmLAvPsyGgTHYDCvKe9tS9yH47jzwEsFVCWe9mP'
@@ -54,9 +46,9 @@ passport.use(new GitHubStrategy({
         token: accessToken
     }
     
-    signWithKey(privKeyTest, JSON.stringify(githubID)).then(signature => {
+    sign.signWithKey(privKeyTest, JSON.stringify(githubID)).then(signature => {
         let ID = {
-            profile: jwt,
+            profile: githubID,
             signature: signature.signature,
             pubKey: signature.pubKey
         }
@@ -65,51 +57,6 @@ passport.use(new GitHubStrategy({
   }
 ));
 //GITHUB
-
-//SIGNING FUNCTIONS
-async function signWithKey(key, message){
-    return new Promise(response => {
-        //CREATING CK OBJECT
-        var ck = CoinKey.fromWif(key, lyraInfo);
-        //CREATE HASH FROM MESSAGE
-        let hash = CryptoJS.SHA256(message);
-        let msg = Buffer.from(hash.toString(CryptoJS.enc.Hex), 'hex');
-        //GETTING PUBKEY FROM PRIVATEKEY
-        let privKey = ck.privateKey
-        //SIGNING MESSAGE
-        const sigObj = secp256k1.sign(msg, privKey)
-        const pubKey = secp256k1.publicKeyCreate(privKey)
-
-        response({
-            signature: sigObj.signature.toString('hex'),
-            pubKey: pubKey.toString('hex')
-        })
-    })
-}
-
-async function returnPubKey(key){
-    return new Promise(response => {
-        //CREATING CK OBJECT
-        var ck = CoinKey.fromWif(key, lyraInfo);
-        //GETTING PUBKEY FROM PRIVATEKEY
-        let privKey = ck.privateKey
-        const pubKey = secp256k1.publicKeyCreate(privKey)
-        response(pubKey)
-    })
-}
-
-async function verifySign(keyhex, sighex, message){
-    return new Promise(response => {
-        //CREATE HASH FROM MESSAGE
-        let hash = CryptoJS.SHA256(message);
-        let msg = Buffer.from(hash.toString(CryptoJS.enc.Hex), 'hex')
-        //VERIFY MESSAGE
-        let signature = Buffer.from(sighex,'hex')
-        let pubKey = Buffer.from(keyhex,'hex')
-        verified = secp256k1.verify(msg, signature, pubKey)
-        response(verified)
-    })
-}
 
 //EXAMPLE
 /*
