f44c64808b3fa223a8c05cdd7e5a7eaa198b89f4
turinglabsorg <turinglabs@criptext.com>
2020-05-26 12:34:27 +0200

    min fix

---
+++ public/precache-manifest.925fc365b3c81eb0d4b28a0920d9df0e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22504aa6a14fa42dcf79",
+    "url": "/css/app.c37e874e.css"
+  },
+  {
+    "revision": "7de2303ba79bf6b1a725",
+    "url": "/css/check.da25f29c.css"
+  },
+  {
+    "revision": "a8fea14514cd575bb22b",
+    "url": "/css/chunk-vendors.5e44303a.css"
+  },
+  {
+    "revision": "699f809484dd4a5aa94b",
+    "url": "/css/home.ab20ab1d.css"
+  },
+  {
+    "revision": "8fc0dce64452da650944",
+    "url": "/css/identities.da25f29c.css"
+  },
+  {
+    "revision": "868df58b2e5382df2f9c",
+    "url": "/css/share.da25f29c.css"
+  },
+  {
+    "revision": "4ee6387b384cd030583fe3cedde1998a",
+    "url": "/email.png"
+  },
+  {
+    "revision": "ef7a02b69836dc8b6a732a54c4200dcb",
+    "url": "/github.png"
+  },
+  {
+    "revision": "ed9bd53f4d29e160c7984aefa4fc3cf2",
+    "url": "/google.png"
+  },
+  {
+    "revision": "2290a621091f248c2072aa8f068302f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22504aa6a14fa42dcf79",
+    "url": "/js/app.dff03f9c.js"
+  },
+  {
+    "revision": "7de2303ba79bf6b1a725",
+    "url": "/js/check.1f6189db.js"
+  },
+  {
+    "revision": "e082c8feab41536e8bd8",
+    "url": "/js/check~identities~share.a32e2fce.js"
+  },
+  {
+    "revision": "a8fea14514cd575bb22b",
+    "url": "/js/chunk-vendors.602ad607.js"
+  },
+  {
+    "revision": "699f809484dd4a5aa94b",
+    "url": "/js/home.0281c147.js"
+  },
+  {
+    "revision": "31daf3c36b1dc43bba77",
+    "url": "/js/home~identities~share.1e73226a.js"
+  },
+  {
+    "revision": "8fc0dce64452da650944",
+    "url": "/js/identities.50c75905.js"
+  },
+  {
+    "revision": "868df58b2e5382df2f9c",
+    "url": "/js/share.0cc4565e.js"
+  },
+  {
+    "revision": "30c453b7f5fbdb09ea0cb42a5dc7a6e5",
+    "url": "/linkedin.png"
+  },
+  {
+    "revision": "a7077221c63ac310e2e8612e2d4549fe",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "b9aa66d6af9ab63a600689ae4fbe460d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "53d8c58b0903c5a33a92f777542e136e",
+    "url": "/phone.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9e1a371b50816593a12ee9bfe4a02db6",
+    "url": "/success.png"
+  },
+  {
+    "revision": "bfa97b73df70d935dafeb87158c019f3",
+    "url": "/twitter.png"
+  }
+]);
